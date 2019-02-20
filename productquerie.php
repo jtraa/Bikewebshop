b7d668e160a7a3a3ad60f5dc853f90f67bb17402
Jelle Traa <jelletraa@gmail.com>
2019-02-20 17:02:49 +0100

    yes please

--- productquerie.php
+++ productquerie.php
@@ -1,6 +1,6 @@
 <?php
     include "db-connection.php";        
-
+ 
     $sql_querie = "SELECT name, color, image, price FROM bikes";
     
     $db_result = $conn->query($sql_querie);  
@@ -16,10 +16,15 @@
              '<h2>' . $row['name']  .'</h2>' .
              '<h6>' . $row['color'] .'</h6>' . 
              '<img src="' . $row['image'] . '" alt="' . '" style="width: 100%">' .
+             
+             '<div id="add" class="cart"><a href=
+             "cart.php?name=' . $row['name'] . 
+             '&color=' . $row['color'] . 
+             '&price=' . $row['price'] .  '"><h2>Reserve</h2></a></div>' .
+
+
              '<h5>'. '€ '  . $row['price']  . ',-' .'</h5>' . 
-             
-             
-            '<div id="add"><a href="addtocart.php"><h2>Reserve</h2></a></div>' .
+            
              
             '</div>';
     }   
